6d8ed827906bbc238ab3cd8329d20acccd634b77
mukhtarcal <raahimmukhtar@berkeley.edu>
2025-08-25 15:56:01 +0800

    Add welcome message and Remove placeholder

---
+++ src/main/java/Dennis.java
@@ -0,0 +1,12 @@
+public class Dennis {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Dennis\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
+        System.out.println(greeting);
+    }
+}
