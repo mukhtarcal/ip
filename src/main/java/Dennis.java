9d836b7e628e8ee6fff52552abc99c7b8f739499
mukhtarcal <raahimmukhtar@berkeley.edu>
2025-09-02 14:38:10 +0800

    Add user input and echo it

--- src/main/java/Dennis.java
+++ src/main/java/Dennis.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class Dennis {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n" +
-                " Hello! I'm Dennis\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+        String line;
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(greeting);
+        System.out.println("____________________________________________________________\n" + " Hello! I'm Dennis\n" + " What do you want?! :|\n" + "____________________________________________________________\n");
+
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println("____________________________________________________________\n" + " " + line + "\n" + "____________________________________________________________\n");
+            line = in.nextLine();
+        }
+
+        System.out.println("____________________________________________________________\n" + " Bye. Hope to see you again soon!\n" + "____________________________________________________________\n");
     }
 }
